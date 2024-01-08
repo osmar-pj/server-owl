492aec8e7ee08f8a346888b99954d0ab1981cf2f
osmarpj <osmar.pj@hotmail.com>
2024-01-08 18:37:34 -0500

    UPDATE config database and version NPM

--- src/database.js
+++ src/database.js
@@ -2,11 +2,12 @@ import mongoose from 'mongoose'
 import { config } from 'dotenv'
 config()
 
-mongoose.connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
+mongoose.connect(process.env.MONGO_URI, {
+    // auth: {
+    //     user: process.env.MONGO_USER,
+    //     password: process.env.MONGO_PASSWORD
+    // },
+    // authSource: 'admin',
 })
 .then((db) => console.log('Database connected oh YEAH'))
 .catch((err) => console.error(err))
